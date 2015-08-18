a951e290bcd0b0c5b743aa475792a4ec37dcd454
m1221 <>
2015-08-18 00:32:14 -0500

    MULTIPLE: prepare game for initial project submission

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
+Udacity Student: Mario_
+GitHub Account: m1221
+Date of Project Submission: 08/2015
 
-for self-checking their submission.
+Instructions for Setup:
+To play the frontend-arcade-game, open index.html in your browser.
+Your objective is to control your character using the down, left, right, and up
+arrow keys on your keyboard and guide him towards the river without colliding
+with any nasty bugs.
+See how high you can get your score!
+Warning, enemies get faster the higher your score goes.
--- js/app.js
+++ js/app.js
@@ -1,19 +1,50 @@
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    // Enemies will be instantiated with different speeds
+    // Speeds are adjusted depending on player score
+    this.speed = speed; 
 }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // Enemies move from left to right.
+    // If an enemy is right of the canvas, the enemy's position
+    // will be set to left of the canvas and the enemy will
+    // continue to move from left to right at the next fnct call
+    if (this.x > 500){
+        this.x = -100;
+    }
+    else{
+        this.x = this.x + (90 * dt) * this.speed;
+    }
+    
+    // Collision detection. 
+    if ((player.x < this.x + 35 && player.x > this.x - 35) &&
+    (player.y < this.y + 30 && player.y > this.y - 50)){
+        // Decrement player score if it is greater than 0
+        // Adjust enemy speed if player score is greater than 0
+        if (player.score > 0){
+            player.score--;
+            document.getElementById('score').textContent= player.score.toString();
+            allEnemies.forEach(function(enemy){
+                enemy.speed = enemy.speed - 0.1;
+            });
+        }
+        // Reset player position to starting position
+        player.x = player.startingPosition[0];
+        player.y = player.startingPosition[1];
+    }
+    
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +55,58 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.startingPosition = [252, 400];
+    this.sprite = 'images/char-boy.png';
+    this.x = this.startingPosition[0];
+    this.y = this.startingPosition[1];
+    this.score = 0;
+    
+}
 
+Player.prototype.update = function() {
+    // can't think of anything that should go here
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(keyMark) {
+    // If player reaches water reset position, add increment score,
+    // increase enemy speed
+    if (this.y < 30){
+      this.x = this.startingPosition[0];
+      this.y = this.startingPosition[1];
+      this.score++;
+      allEnemies.forEach(function(enemy){
+          enemy.speed = enemy.speed + 0.1;
+      });
+      document.getElementById('score').textContent= this.score.toString();
+    }
+    // Adjust player position depending on keyboard input
+    // and location of player
+    if (keyMark === 'left' && this.x >= -18){
+        this.x = this.x - 30;
+    }
+    else if (keyMark === 'right' && this.x <= 402){
+        this.x = this.x + 30;
+    }
+    else if (keyMark === 'up' ){
+        this.y = this.y - 30;
+    }
+    else if (keyMark === 'down' && this.y <= 370){
+        this.y = this.y + 30;
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+player = new Player();
+allEnemies = [new Enemy(-50, 50, 1.3), new Enemy(-200, 50, 1.3), new Enemy(-375, 50, 1.3),
+new Enemy(-50, 140, 0.8), new Enemy(-300, 140, 0.8), new Enemy(-425, 140, 0.8), 
+new Enemy(300, 225, 1), new Enemy(100, 225, 1), new Enemy(-100, 225, 1)];
 
 
 
