da413633c7e56ad7fe90274dfa5e9a2b4d87b736
m1221 <>
2015-08-18 09:17:34 -0500

    style: add semicolons, also add 'use strict'

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+'use strict';
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
@@ -12,7 +12,7 @@ var Enemy = function(x, y, speed) {
     // Enemies will be instantiated with different speeds
     // Speeds are adjusted depending on player score
     this.speed = speed; 
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -45,12 +45,12 @@ Enemy.prototype.update = function(dt) {
         player.y = player.startingPosition[1];
     }
     
-}
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -62,15 +62,15 @@ var Player = function() {
     this.y = this.startingPosition[1];
     this.score = 0;
     
-}
+};
 
 Player.prototype.update = function() {
     // can't think of anything that should go here
-}
+};
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 Player.prototype.handleInput = function(keyMark) {
     // If player reaches water reset position, add increment score,
@@ -98,7 +98,7 @@ Player.prototype.handleInput = function(keyMark) {
     else if (keyMark === 'down' && this.y <= 370){
         this.y = this.y + 30;
     }
-}
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
